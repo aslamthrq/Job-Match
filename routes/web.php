fb479c96e614893031883673f8c40c5eebc86c4f
aslam thariq <aslamthariq01@gmail.com>
2024-05-19 13:52:25 +0700

    Nonaktifkan beberapa route

--- routes/web.php
+++ routes/web.php
@@ -6,14 +6,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('landingPage');
 });
-Route::get('/login', function () {
-    return view('login');
-});
-Route::get('/register', function () {
-    return view('register');
-});
+// Route::get('/login', function () {
+//     return view('login');
+// });
+// Route::get('/register', function () {
+//     return view('register');
+// });
 
-Route::get('/dashboard', [recruiterController::class, 'index'])->name('dashboard');
-Route::get('/postroom', [recruiterController::class, 'postroom'])->name('postroom');
-Route::get('/postroom1', [recruiterController::class, 'detailPostroom'])->name('detailPostroom');
-Route::get('/profile', [recruiterController::class, 'profile'])->name('profile');
+// Route::get('/dashboard', [recruiterController::class, 'index'])->name('dashboard');
+// Route::get('/postroom', [recruiterController::class, 'postroom'])->name('postroom');
+// Route::get('/postroom1', [recruiterController::class, 'detailPostroom'])->name('detailPostroom');
+// Route::get('/profile', [recruiterController::class, 'profile'])->name('profile');
